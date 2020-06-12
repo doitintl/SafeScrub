c0b213b8ade8e75cd3ba4bfd398a99c8f4608745
Joshua Fox <4542591+JoshuaFox@users.noreply.github.com>
2020-06-12 11:02:16 +0300

    cleanup

--- run-scrubber.sh
+++ run-scrubber.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-./scrubber.sh -a project-viewer@joshua-playground.iam.gserviceaccount.com -k project-viewer-credentials.json -p joshua-playground
+./scrubber.sh -a project-viewer@joshua-playground.iam.gserviceaccount.com -k project-viewer-credentials.json -p joshua-playground 2>>errors.txt | \
+ grep -v "firewalls\/default" | \
+ grep -v "networks\/defaults"
