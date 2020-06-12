12fdb1267ae40ec78d2c3b796b963685437e0243
Joshua Fox <4542591+JoshuaFox@users.noreply.github.com>
2020-06-12 12:23:17 +0300

    delete run-scrubber.sh

--- run-scrubber.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./scrubber.sh -a project-viewer@joshua-playground.iam.gserviceaccount.com -k project-viewer-credentials.json -p joshua-playground 2>>errors.txt | \
- grep -v "firewalls\/default" | \
- grep -v "networks\/defaults"
