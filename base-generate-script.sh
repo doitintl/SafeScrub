fdaff864f927e9b38cd29f715cf9fe850e4a9770
Joshua Fox <4542591+JoshuaFox@users.noreply.github.com>
2020-06-14 18:27:06 +0300

    allow async operation

--- base-generate-script.sh
+++ base-generate-script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -x
-
 login() {
   gcloud -q auth activate-service-account "${account_name}" --key-file "${key_file}" || exit 1
   gcloud -q config set project "${project_id}" || exit 1
@@ -20,6 +18,8 @@ Usage:
   -k filename for credentials file
   -p project id or account
   -f filter expression. Run gcloud topics filter for documentation. This filter is not supported on Google Storage.
+  -b to generate a deletion script that runs all commands asynchronously (potentially concurrently). This will speed up deletion.
+  -h help
 
 You can direct output to create your deletion script, as for example by  suffixing
        > deletion-script.sh  && chmod a+x deletion-script.sh
@@ -38,19 +38,22 @@ create_deletion_code() {
   for resource_type in "${resource_types_array[@]}"; do
     echo >&2 "Listing ${gcloud_component} ${resource_type}"
     local resources
-    resources="$(gcloud -q "${gcloud_component}" "${resource_type}" list --filter "${filter}" --uri) "
+    resources="$(gcloud -q "${gcloud_component}" "${resource_type}" list --filter "${filter}" --uri)"
     local resources_array=()
     # shellcheck disable=SC2207
     resources_array=($(echo "$resources" | tr ' ' '\n'))
+    if [ -n "${resources_array}" ]; then
+     echo >&2 "Listed ${#resources_array[@]} ${gcloud_component} ${resource_type}"
+    fi
     local resource
     for resource in "${resources_array[@]}"; do
-      echo "gcloud ${gcloud_component} ${resource_type} delete -q ${resource}"
+      echo "gcloud ${gcloud_component} ${resource_type} delete -q ${resource} $async_ampersand"
     done
   done
 }
 
 create_cloud_functions_deletion_code() {
-  echo >&2 "Listing Functions"
+  echo >&2 "Listing cloud functions"
   local funcs
   funcs=$(gcloud -q functions list --filter "${filter}" --format="table[no-heading](name)" )
   # shellcheck disable=SC2207
@@ -62,7 +65,7 @@ create_cloud_functions_deletion_code() {
 }
 
 create_bucket_deletion_code() {
-  echo >&2 "Listing Buckets"
+  echo >&2 "Listing buckets"
   local buckets
   buckets="$(gsutil ls)"
   # shellcheck disable=SC2207
@@ -73,7 +76,7 @@ create_bucket_deletion_code() {
   done
 }
 
-while getopts 'k:p:a:f:' OPTION; do
+while getopts 'k:p:a:f:b' OPTION; do
   case "$OPTION" in
   k)
     key_file="$OPTARG"
@@ -86,6 +89,9 @@ while getopts 'k:p:a:f:' OPTION; do
     ;;
   f)
     filter="$OPTARG"
+    ;;
+  b)
+    async_ampersand="&"
     ;;
   ?)
     usage
@@ -104,9 +110,7 @@ create_bucket_deletion_code
 create_cloud_functions_deletion_code
 
 create_deletion_code pubsub "subscriptions topics snapshots"
-
-compute_resource_types="addresses backend-services firewall-rules forwarding-rules health-checks http-health-checks https-health-checks instance-groups instance-templates instances networks routers routes target-pools target-tcp-proxies"
+compute_resource_types="instances backend-services firewall-rules forwarding-rules health-checks http-health-checks https-health-checks instance-groups instance-templates routers routes target-pools target-tcp-proxies networks"
 create_deletion_code compute "${compute_resource_types}"
-
 create_deletion_code sql instances
 create_deletion_code app "services versions instances firewall-rules"  # services covers versions and instances but we want to generate a list for human review
