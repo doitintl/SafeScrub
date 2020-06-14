de0eda8c03bbcf36c38466f8781c6f8dd546651b
Joshua Fox <4542591+JoshuaFox@users.noreply.github.com>
2020-06-14 18:06:03 +0300

    uri param

--- base-generate-script.sh
+++ base-generate-script.sh
@@ -1,42 +1,75 @@
 #!/bin/bash
+set -x
 
-trap "exit" INT
-function login() {
+login() {
   gcloud -q auth activate-service-account "${account_name}" --key-file "${key_file}" || exit 1
   gcloud -q config set project "${project_id}" || exit 1
 }
 
-function usage() {
- >&2 echo "./generate-script -a my-service-account@myproject.iam.gserviceaccount.com -k project-viewer-credentials.json -p  my-project [-f filter-expression]"
->&2  echo "You can direct output to create your deletion script, as for example by  suffixing"
- >&2 echo "        > deletion-script.sh  && chmod a+x deletion-script.sh"
- >&2 printf "Resources from these APIs are supported: "
+usage() {
+  local this_script
   this_script=$(basename "$0")
->&2  grep "^\s*create_deletion_code" "${this_script}" | cut -d " " -f2 | tr '\n' ', ' | rev | cut -c 2- | rev
+  local supported
+  supported=$(grep "^\s*create_deletion_code " "${this_script}" | cut -d " " -f2 | tr '\n' ', ' | rev | cut -c 2- | rev)
+  supported="${supported} storage,functions"
+
+  cat <<EOD >&2
+Usage:
+./generate-script -a my-service-account@myproject.iam.gserviceaccount.com -k project-viewer-credentials.json -p  my-project [-f filter-expression]"
+  -a service account
+  -k filename for credentials file
+  -p project id or account
+  -f filter expression. Run gcloud topics filter for documentation. This filter is not supported on Google Storage.
+
+You can direct output to create your deletion script, as for example by  suffixing
+       > deletion-script.sh  && chmod a+x deletion-script.sh
+
+Resources from these APIs are supported: ${supported}
+EOD
   exit 1
 }
 
-function create_deletion_code() {
-  gcloud_component=$1
-  resource_types=$2
-  get_uri=$3
-  if [ "true" = "${get_uri}" ]; then
-    uri_option="--uri"
-  else
-    uri_option=""
-  fi
+create_deletion_code() {
+  local gcloud_component=$1
+  local resource_types=$2
 
   # shellcheck disable=SC2207
-  resource_types_array=($(echo "$resource_types" | tr ' ' '\n'))
+  local resource_types_array=($(echo "$resource_types" | tr ' ' '\n'))
   for resource_type in "${resource_types_array[@]}"; do
-    >&2 echo "Listing ${gcloud_component} ${resource_type}"
-    resource_uris="$(gcloud -q "${gcloud_component}" "${resource_type}" list --filter "${filter}" ${uri_option})"
-    resource_uri_array=()
+    echo >&2 "Listing ${gcloud_component} ${resource_type}"
+    local resources
+    resources="$(gcloud -q "${gcloud_component}" "${resource_type}" list --filter "${filter}" --uri) "
+    local resources_array=()
     # shellcheck disable=SC2207
-    resource_uri_array=($(echo "$resource_uris" | tr ' ' '\n'))
-    for resource_uri in "${resource_uri_array[@]}"; do
-      echo "gcloud ${gcloud_component} ${resource_type} delete -q ${resource_uri}"
+    resources_array=($(echo "$resources" | tr ' ' '\n'))
+    local resource
+    for resource in "${resources_array[@]}"; do
+      echo "gcloud ${gcloud_component} ${resource_type} delete -q ${resource}"
     done
+  done
+}
+
+create_cloud_functions_deletion_code() {
+  echo >&2 "Listing Functions"
+  local funcs
+  funcs=$(gcloud -q functions list --filter "${filter}" --format="table[no-heading](name)" )
+  # shellcheck disable=SC2207
+  local funcs_array=($(echo "${funcs}" | tr ' ' '\n'))
+  local func
+  for func in "${funcs_array[@]}"; do
+    echo "gcloud functions delete -q ${func}"
+  done
+}
+
+create_bucket_deletion_code() {
+  echo >&2 "Listing Buckets"
+  local buckets
+  buckets="$(gsutil ls)"
+  # shellcheck disable=SC2207
+  local buckets_array=($(echo "${buckets}" | tr ' ' '\n'))
+  local bucket
+  for bucket in "${buckets_array[@]}"; do
+    echo "gsutil rm -r ${bucket}" # $bucket variable is in the form gs://bucketname
   done
 }
 
@@ -66,14 +99,14 @@ fi
 
 login
 
-create_deletion_code pubsub "subscriptions topics snapshots" true
+create_deletion_code container clusters
+create_bucket_deletion_code
+create_cloud_functions_deletion_code
+
+create_deletion_code pubsub "subscriptions topics snapshots"
 
 compute_resource_types="addresses backend-services firewall-rules forwarding-rules health-checks http-health-checks https-health-checks instance-groups instance-templates instances networks routers routes target-pools target-tcp-proxies"
-create_deletion_code compute "${compute_resource_types}" true
+create_deletion_code compute "${compute_resource_types}"
 
-create_deletion_code container clusters false
-
-create_deletion_code sql instances true
-create_deletion_code app "services versions instances firewall-rules" true # services covers versions and instances but we want to generate a list for human review
-# TODO gsutil list
-# TODO (no resource type) create_deletion_code  functions "" true
+create_deletion_code sql instances
+create_deletion_code app "services versions instances firewall-rules"  # services covers versions and instances but we want to generate a list for human review
--- generate-script.sh
+++ generate-script.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+# Calls the base script, and also
+# 1. Directs the output through a filter so that items in  "no-delete.txt" are not in the ultimate output script
+# 2. Reverts the logged-in user to what it was before the script is run.
+function revert_authentication() {
+  gcloud config set account ${original_account}
+  gcloud config set project ${original_project}
+}
+
+trap "cleanup" INT
+
+original_account=$(gcloud auth list --filter=status:ACTIVE --format="value(account)")
+original_project=$(gcloud config get-value project)
+
 ./base-generate-script.sh "$@" |
   grep -v -f no-delete.txt
+
+revert_authentication
