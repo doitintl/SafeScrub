c8f1329e439b43640fdd5cfd12cddef397dc8be0
Joshua Fox <4542591+JoshuaFox@users.noreply.github.com>
2020-06-14 15:48:03 +0300

    errors to stderr

--- base-generate-script.sh
+++ base-generate-script.sh
@@ -7,12 +7,12 @@ function login() {
 }
 
 function usage() {
-  echo "./generate-script -a my-service-account@myproject.iam.gserviceaccount.com -k project-viewer-credentials.json -p  my-project [-f filter-expression]"
-  echo "You can direct output to create your deletion script, as for example by  suffixing"
-  echo "        > deletion-script.sh  && chmod a+x deletion-script.sh"
-  printf "Resources from these APIs are supported: "
+ >&2 echo "./generate-script -a my-service-account@myproject.iam.gserviceaccount.com -k project-viewer-credentials.json -p  my-project [-f filter-expression]"
+>&2  echo "You can direct output to create your deletion script, as for example by  suffixing"
+ >&2 echo "        > deletion-script.sh  && chmod a+x deletion-script.sh"
+ >&2 printf "Resources from these APIs are supported: "
   this_script=$(basename "$0")
-  grep  "^\s*create_deletion_code" "${this_script}"| cut -d " " -f2 | tr '\n' ', ' | rev | cut -c 2- | rev
+>&2  grep "^\s*create_deletion_code" "${this_script}" | cut -d " " -f2 | tr '\n' ', ' | rev | cut -c 2- | rev
   exit 1
 }
 
@@ -29,6 +29,7 @@ function create_deletion_code() {
   # shellcheck disable=SC2207
   resource_types_array=($(echo "$resource_types" | tr ' ' '\n'))
   for resource_type in "${resource_types_array[@]}"; do
+    >&2 echo "Listing ${gcloud_component} ${resource_type}"
     resource_uris="$(gcloud -q "${gcloud_component}" "${resource_type}" list --filter "${filter}" ${uri_option})"
     resource_uri_array=()
     # shellcheck disable=SC2207
@@ -38,6 +39,7 @@ function create_deletion_code() {
     done
   done
 }
+
 while getopts 'k:p:a:f:' OPTION; do
   case "$OPTION" in
   k)
@@ -62,11 +64,11 @@ if [[ -z ${account_name} || (-z ${key_file} || -z ${project_id}) ]]; then
   usage
 fi
 
-#login
+login
 
 create_deletion_code pubsub "subscriptions topics snapshots" true
-exit 0
-compute_resource_types="addresses backend-services firewall-rules forwarding-rules health-checks http-health-checks https-health-checks instance-groups instance-templates instances networks networks routers routes target-pools target-tcp-proxies"
+
+compute_resource_types="addresses backend-services firewall-rules forwarding-rules health-checks http-health-checks https-health-checks instance-groups instance-templates instances networks routers routes target-pools target-tcp-proxies"
 create_deletion_code compute "${compute_resource_types}" true
 
 create_deletion_code container clusters false
