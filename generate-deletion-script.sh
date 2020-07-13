e1a66aca5c16d219c0b82b18b6a1270f21991af6
Joshua Fox <4542591+JoshuaFox@users.noreply.github.com>
2020-07-13 13:04:26 +0300

    Support blank lines in exclusions.txt

--- generate-deletion-script.sh
+++ generate-deletion-script.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+exclusions_file=exclusions.txt
+temp_exclusions_file="${exclusions_file}.tmp"
+
 # Calls the base script, and also
 # 1. Directs the output through a filter so that items in  "exclusions.txt" are not in the ultimate output script
 # 2. On exit, reverts the logged-in user to what it was before the script is run.
-function revert_authentication() {
+function revert() {
   gcloud config set account "${original_account}"
   gcloud config set project "${original_project}"
+
+  rm ${temp_exclusions_file} ||true
 }
 
-trap "revert_authentication" INT
+trap "revert" INT
 
 original_account=$(gcloud auth list --filter=status:ACTIVE --format="value(account)")
 original_project=$(gcloud config get-value project)
-exclusions_file=exclusions.txt
+
+# Remove blank lines from exclusions.txt as these would cause everything to be excluded
+grep -e "\S" ${exclusions_file} > ${temp_exclusions_file}
+
 # If exclusions.txt file is empty, add a comment. This is to prevent grep -v -f from failing the whole script
-grep -q '[^[:space:]]' < "${exclusions_file}" || echo "# No excluded resources" > ${exclusions_file}
+grep -q '[^[:space:]]' <"${temp_exclusions_file}" || echo "# No excluded resources" >${temp_exclusions_file}
 
-./base-gen-deletion-script.sh "$@" | grep -v -f ${exclusions_file}
+./base-gen-deletion-script.sh "$@" | grep -v -f ${temp_exclusions_file}
 
-revert_authentication
+revert
